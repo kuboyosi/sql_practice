4850a217c1bc646edf2211eded751d199744d88c
yoshikura.kubo <kuboyosi@gmail.com>
2022-04-23 05:43:11 +0900

    Create 012_chap05.sql

---
+++ 012_chap05.sql
@@ -0,0 +1,9 @@
+SELECT 
+    s.product_id AS product_id
+    , SUM(s.quantity) AS sum_qty
+    , SUM(s.quantity) / (SELECT SUM(quantity) FROM `sql-book-336704.sample.sales`) AS sales_share
+FROM `sql-book-336704.sample.sales` AS s
+INNER JOIN `sql-book-336704.sample.products` AS p
+ON s.product_id = p.product_id
+GROUP BY product_id
+ORDER BY sales_share
