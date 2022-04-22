3d9ceb03e27b0596ec0687dc92b478ac56e8dc58
yoshikura.kubo <kuboyosi@gmail.com>
2022-04-23 06:28:43 +0900

    Update 015_chap05.sql

--- chapters/015_chap05.sql
+++ chapters/015_chap05.sql
@@ -4,8 +4,8 @@ WITH sales_with_cost AS (
         , p.product_name
         , s.quantity
         , s.revenue
-        , p.cost
-        , s.revenue - p.cost AS profit
+        , p.cost * s.quantity AS cost
+        , s.revenue - p.cost * s.quantity AS profit
     FROM `sql-book-336704.sample.sales` AS s
     LEFT OUTER JOIN `sql-book-336704.sample.products` AS p
     ON s.product_id = p.product_id
@@ -19,4 +19,4 @@ SELECT
     , SUM(profit) AS sum_profit
 FROM sales_with_cost
 GROUP BY product_name
-ORDER BY sum_profit DESC
+ORDER BY sum_profit DESC LIMIT 3
