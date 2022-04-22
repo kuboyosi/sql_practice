b09582e4ec3bf3b845813ba0b308d66e5421b2fe
yoshikura.kubo <kuboyosi@gmail.com>
2022-04-23 05:48:09 +0900

    Update 012_chap05.sql

--- 012_chap05.sql
+++ 012_chap05.sql
@@ -6,4 +6,4 @@ FROM `sql-book-336704.sample.sales` AS s
 INNER JOIN `sql-book-336704.sample.products` AS p
 ON s.product_id = p.product_id
 GROUP BY product_id
-ORDER BY sales_share
+ORDER BY sales_share DESC LIMIT 5
